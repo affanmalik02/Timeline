d8e9195f8151113913c4498864da9d3c389b7645
affanmalik02 <affanmalik02@yahoo.com>
2023-12-01 21:03:27 -0500

    MongoDB update

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,4 @@
 # Stores all configuration values
-SECRET_KEY = b'Aq8\xc0\xb3\xefp\x03\xe6=\xbd)\x82T}\xdc'
-MONGODB_HOST = 'mongodb+srv://affan:ebsKBvORFmfdnVra@cluster0.6acdati.mongodb.net/?retryWrites=true&w=majority'
+SECRET_KEY = b''
+MONGODB_HOST = ''
 
