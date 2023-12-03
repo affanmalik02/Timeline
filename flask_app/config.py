7031f45d4a797972db399e6fd3399d5dd7e6b4ed
affanmalik02 <affanmalik02@yahoo.com>
2023-12-03 13:24:58 -0500

    site overhaul

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,5 @@
 # Stores all configuration values
-SECRET_KEY = b'Aq8\xc0\xb3\xefp\x03\xe6=\xbd)\x82T}\xdc'
-MONGODB_HOST = 'mongodb+srv://affan:ebsKBvORFmfdnVra@cluster0.6acdati.mongodb.net/?retryWrites=true&w=majority'
+SECRET_KEY = ''
+MONGODB_HOST = ''
 
+#Also configure Flask-Mail in routes.py
